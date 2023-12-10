1d39a9856e1b60a7d33ecc62f11128855a75b45a
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-12-10 21:46:09 +0000

    commit

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -1,8 +1,9 @@
-const { createProduct, getProducts, updateProduct, deleteProduct, searchProduct } = require('../controllers/productsController');
+const { createProduct, getProducts, updateProduct, deleteProduct, searchProduct, addMutiples } = require('../controllers/productsController');
 
 const router = require('express').Router();
 
 router.post('/', createProduct);
+router.post('/multiples', addMutiples);
 router.get('/', getProducts);
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
