4c6e992538a7cb3572ba4af6a633a3ae8011b2b2
TijanAyo <ayomidetijani6@gmail.com>
2023-12-23 15:24:05 +0100

    Change: Helper functions and utilis

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,25 +1,38 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const routes = require("./routes");
-const cors = require("cors");
-const { connectDb } = require("./config/db");
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import Appresponse from "./helper/appResponse.js";
 dotenv.config();
-const port = process.env.PORT | 8088;
 
-connectDb();
+// Routes
+import AuthRoute from "./routes/auth.js";
 
-//create express app
 const app = express();
-//add middleware
-app.use(cors());
-app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 //use routes
-app.use("/api", routes);
+app.use("/api/auth", AuthRoute);
 
-app.use("/api/product", require("./routes/product"));
-
-//listen to port
-app.listen(port, () => {
-  console.log(`server is runnning on port: ${port}`);
+// Default route
+app.get("/", (_req, res) => {
+  return Appresponse(res, 200, "Delah Backend API");
 });
+
+// Catch-all 404 route
+app.use("*", (_req, res) => {
+  return Appresponse(
+    res,
+    404,
+    "The specified end-point does not exist. Check request method or route and try again."
+  );
+});
+
+export default app;
